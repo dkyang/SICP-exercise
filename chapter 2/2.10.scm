17206fd76f500d3c28fa525153ffbbe4de9032d9
dkyang <yangzhuangsuan@gmail.com>
2014-06-01 11:36:29 +0800

    add 2.10.scm

---
+++ chapter 2/2.10.scm
@@ -0,0 +1,28 @@
+#lang planet neil/sicp
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound x) (car x))
+
+(define (upper-bound x) (cdr x))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (and (< (lower-bound y) 0) (> (upper-bound y) 0))
+      (error "y should not cross zero point")
+      (mul-interval x 
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
+
+; test
+(div-interval (make-interval 1 2) (make-interval 1 4))
+(div-interval (make-interval 1 2) (make-interval -1 4))
+
+
