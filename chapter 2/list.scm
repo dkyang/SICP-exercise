73f228141271783ace8d52c72fda4b7190ff36d6
dkyang <yangzhuangsuan@gmail.com>
2014-06-07 10:30:28 +0800

    add list.scm

---
+++ chapter 2/list.scm
@@ -0,0 +1,24 @@
+#lang planet neil/sicp
+
+(list 1 2 3 4)
+(cons 1 (cons 2 (cons 3 (cons 4 nil))))
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
+
+(list-ref squares 3)
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+
+(length odds)
+
+(append squares odds)
