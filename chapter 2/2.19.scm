564444104fdf4ed03301a38d898980d2c454191d
dkyang <yangzhuangsuan@gmail.com>
2014-06-07 14:49:11 +0800

    add 2.19.scm

---
+++ chapter 2/2.19.scm
@@ -0,0 +1,52 @@
+#lang planet neil/sicp
+
+#|
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount 
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+|#
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define us-coins (list 50 25 10 5 1))
+
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (first-denomination coin-values)
+  (car coin-values))  
+
+(cc 100 us-coins)
+
+; 无论coin-valus的顺序怎么排列，都不会对结果产生影响。
+; 因为这种算法的原理是将问题分为在当前零钱总数时使用某种硬币及不使用
+; 某种硬币这两个互补的情况，然后递归将每种情况的找零钱个数
+; 相加，这样的一个过程与对每种硬币的判断顺序无关，因此也与
+; coin-values的排列顺序无关。
+
